ea9e741e23183684cc916c770d8647cf37825ea4
vikas sharma <vikas@lintelindia.com>
2019-12-26 16:18:09 +0530

    first commit

---
+++ ReturnStringPod/ReturnStringPod/ReturnStringPod.h
@@ -0,0 +1,19 @@
+//
+//  ReturnStringPod.h
+//  ReturnStringPod
+//
+//  Created by Harsh Vardhan on 26/12/19.
+//  Copyright © 2019 Harsh Vardhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ReturnStringPod.
+FOUNDATION_EXPORT double ReturnStringPodVersionNumber;
+
+//! Project version string for ReturnStringPod.
+FOUNDATION_EXPORT const unsigned char ReturnStringPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReturnStringPod/PublicHeader.h>
+
+
---
+++ ReturnStringPod/ReturnStringPod/Service.swift
@@ -0,0 +1,17 @@
+//
+//  Service.swift
+//  ReturnStringPod
+//
+//  Created by Harsh Vardhan on 26/12/19.
+//  Copyright © 2019 Harsh Vardhan. All rights reserved.
+//
+
+import Foundation
+
+public class Service{
+    private init(){}
+    
+    public static func doSomething() -> String{
+        return "Hello this is pod test string"
+    }
+}
